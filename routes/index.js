eec8ad60467ef5a4f54c12b19130e82ed18bb467
Daniel Ventura <ventura.daniel36@gmail.com>
2020-02-23 19:35:54 -0700

    Initial Setup for Theater Web Dev

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+//created the router for the whole website
+const router = express.Router()
+
+
+router.get('/', (req, res) => {
+    res.render('index')
+})
+
+//exports the router so server can read it
+module.exports = router
