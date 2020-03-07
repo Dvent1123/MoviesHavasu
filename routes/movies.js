c91fe3c1309387ec7e5c7c8645a17c84e9ddc0a9
Daniel Ventura <ventura.daniel36@gmail.com>
2020-03-06 17:39:56 -0700

    updated views

--- routes/movies.js
+++ routes/movies.js
@@ -2,12 +2,41 @@ const express = require('express')
 //created the router for the whole website
 const router = express.Router()
 
-router.get('/', (req,res) =>{
+// router.get('/sort', (req, res) =>{
+//     console.log(req.query.Test)
+// })
+
+router.get('/search', (req,res) =>{
     var app = req.app;
     const allMoviesArray = (app.get('allMovies'))
-    let query = allMoviesArray.find(movie => movie.title == req.query.title)
-    console.log(query.title)
+
+    // let searchOptions = {}
+
+    // searchOptions.title = new RegExp(req.query.title, 'i')
+    // console.log(searchOptions.title)
+    // console.log(searchOptions)
+    // //const movie = allMoviesArray.find(searchOptions)
+
+    //console.log(movie)
+    
+    // let query = allMoviesArray.find(movie => movie.title == req.query.title)
+    // console.log(query.title)
     //this ^ works just have to find a way to use regex to query the data and then pass into movie shit
+    
+    // let regexp
+    // let resultMovie 
+    // let test = allMoviesArray.find(movie => 
+    // {   regexp =  new RegExp(movie.title, 'i')
+    //     resultMovie = regexp.exec(req.query.title)
+    //     console.log(resultMovie)
+    // })
+    //code below also works but only when exact words are typed
+
+
+
+    //code below makes new regexpression and we want to find what is inside
+    //code below will take the query and execute on the string which is what the user input
+
     // if(req.query.title != null && req.query.title != ''){
     //     query = query.regex('title', new RegExp(req.query.title, 'i'))
     // }
@@ -23,6 +52,72 @@ router.get('/', (req,res) =>{
     // }
 })
 
+router.get('/', async (req, res) => {
+    var app = req.app;
+    const allMoviesArray = (app.get('allMovies'))
+
+    //this is rendering this page but it's still at /
+    res.render('movies/index', {allMoviesArray: allMoviesArray})
+
+    if(req.query.userSubmit != null){
+        console.log(req.query.userSubmit)
+        let newMovieArray = await sortChoice(req.query.userSubmit, allMoviesArray)
+        console.log(newMovieArray)
+
+        await newMovieArray.forEach(movie => {
+            if(movie._id == req.params._id){
+                res.render('movies/show',{movie: movie})
+            }
+        })
+    }
+
+})
+
+async function sortChoice(userChoice, unsortedMovieArray){
+    let sortedMovieArray = []
+    if(userChoice === 'alphabetical'){
+        sortedMovieArray = sortTest('title', unsortedMovieArray)
+    }
+    if(userChoice === 'rating'){
+        sortedMovieArray = sortTest('rating', unsortedMovieArray)
+    }
+    if(userChoice === 'year'){
+        sortedMovieArray = sortTest('release_date', unsortedMovieArray)
+    }
+    return sortedMovieArray
+}
+
+function sortTest(sortBy, unsortedMovieArray){
+    let sortedArray = unsortedMovieArray
+    return sortedArray.sort(compareValues(sortBy))
+}
+
+function compareValues(key, order = 'asc') {
+    return function innerSort(a, b) {
+      if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+        // property doesn't exist on either object
+        return 0;
+      }
+  
+      const varA = (typeof a[key] === 'string')
+        ? a[key].toUpperCase() : a[key];
+      const varB = (typeof b[key] === 'string')
+        ? b[key].toUpperCase() : b[key];
+  
+      let comparison = 0;
+      if (varA > varB) {
+        comparison = 1;
+      } else if (varA < varB) {
+        comparison = -1;
+      }
+      return (
+        (order === 'desc') ? (comparison * -1) : comparison
+      );
+    };
+  }
+
+
+
 router.get('/:_id', async (req, res) => {
     var app = req.app;
     const allMoviesArray = (app.get('allMovies'))
@@ -30,7 +125,7 @@ router.get('/:_id', async (req, res) => {
     //why does strict equality not work
     await allMoviesArray.forEach(movie => {
         if(movie._id == req.params._id){
-            res.render('movies/index',{movie: movie})
+            res.render('movies/show',{movie: movie})
         }
     })
 })
